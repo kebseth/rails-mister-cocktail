e6fd1a8a0ae02fd9ee205698fdb5a8bfa50b3769
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-08-09 13:55:13 +0200

    15eme deploy

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,4 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
-  validates :image_url, presence: true
 end
